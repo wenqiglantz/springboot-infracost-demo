4644c36536a5bf6970a6a103c2b41a25de3f54e2
Wenqi Glantz <wenqiglantz@gmail.com>
2023-01-09 23:07:02 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,9 +5,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.39.0"
+      version = "4.49.0"
     }
   }
+  required_version = "~> 1.3.6"
 }
 
 provider "aws" {
